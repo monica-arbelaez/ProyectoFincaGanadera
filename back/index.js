99eba8a075e6b79f4d29a63d5620cf35e5db0856
DanielAristy <danielaristy22@gmail.com>
2021-07-25 14:33:05 -0500

    Se cambia la validacion en el objeto de valor Age del agregado Animal, para recibir la fecha en el formato de DD/MM/YYYY

--- back/index.js
+++ back/index.js
@@ -1,44 +1,44 @@
-console.log("Hello");
+// console.log("Hello");
 
-const Pasture = require("./lib/domain/pasture/model/pasture");
-const Density = require("./lib/domain/pasture/model/values/density");
-const newDensity = new Density(88);
-const updateDensity = new Density(55);
-const Area = require("./lib/domain/pasture/model/values/area");
-const newArea = new Area(6);
-const Length = require("./lib/domain/pasture/model/values/length");
-const newLength = new Length(5.6);
+// const Pasture = require("./lib/domain/pasture/model/pasture");
+// const Density = require("./lib/domain/pasture/model/values/density");
+// const newDensity = new Density(88);
+// const updateDensity = new Density(55);
+// const Area = require("./lib/domain/pasture/model/values/area");
+// const newArea = new Area(6);
+// const Length = require("./lib/domain/pasture/model/values/length");
+// const newLength = new Length(5.6);
 
-const Fence = require("./lib/domain/pasture/model/entities/fence");
-const PastureRepositoryMongo = require("./lib/infrastruture/pasture/repositories/pasture-repository-mongo");
-const newFence = new Fence(newLength)
-console.log(newFence)
+// const Fence = require("./lib/domain/pasture/model/entities/fence");
+// const PastureRepositoryMongo = require("./lib/infrastruture/pasture/repositories/pasture-repository-mongo");
+// const newFence = new Fence(newLength)
+// console.log(newFence)
 
-const updataArea = new Area(25);
-const updateLength = new Length(7)
-newFence.updateLength(updateLength)
-console.log(newFence)
+// const updataArea = new Area(25);
+// const updateLength = new Length(7)
+// newFence.updateLength(updateLength)
+// console.log(newFence)
 
-const potrero = new Pasture(null, newArea);
-potrero.associateGrass(newDensity);
-potrero.associateFence(newLength);
-potrero.updateDensityGrass(updateDensity);
-potrero.updateLengthFence(newLength);
-potrero.updateArea(updataArea)
-console.log(potrero);
+// const potrero = new Pasture(null, newArea);
+// potrero.associateGrass(newDensity);
+// potrero.associateFence(newLength);
+// potrero.updateDensityGrass(updateDensity);
+// potrero.updateLengthFence(newLength);
+// potrero.updateArea(updataArea)
+// console.log(potrero);
 
-const newrepository = new PastureRepositoryMongo;
-newrepository.createPasture(potrero)
+// const newrepository = new PastureRepositoryMongo;
+// newrepository.createPasture(potrero)
 
 // const AgreggateId = require("./lib/domain/agreggate-id");
 // const Animal = require("./lib/domain/animal/model/animal");
 // const Age = require("./lib/domain/animal/model/values/age");
 // const Breed = require("./lib/domain/animal/model/values/breed");
-// const Date = require("./lib/domain/animal/model/values/date");
+const Date = require("./lib/domain/animal/model/values/date");
 // const Gender = require("./lib/domain/animal/model/values/gender");
 // const IsVaccinated = require("./lib/domain/animal/model/values/is-vaccinated");
-// const date = new Date('02/31/2021');
-// console.log(date);
+const date = new Date('01/02/2021');
+console.log(date);
 
 // const vacunado = new IsVaccinated(true);
 // console.log(vacunado);
--- back/lib/domain/animal/model/values/date.js
+++ back/lib/domain/animal/model/values/date.js
@@ -1,11 +1,11 @@
 class Date {
     constructor(date) {
-        const validateDate = /^(?:0?[1-9]|1[1-2])([\-/.])(3[01]|[12][0-9]|0?[1-9])\1\d{4}$/
+        const validateDate = /^(?:3[01]|[12][0-9]|0?[1-9])([\-/.])(0?[1-9]|1[1-2])\1\d{4}$/
         if (date === undefined) {
             throw new Error("Debes tener una fecha de vacunación");
         }
         if (!date.match(validateDate)) {
-            throw new Error("La fecha debe tener formato -> MM/DD/YYYY")
+            throw new Error("La fecha debe tener formato -> DD/MM/YYYY")
         }
         this.date = date;
     }
