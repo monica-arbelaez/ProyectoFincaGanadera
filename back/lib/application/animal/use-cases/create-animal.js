e49d7e7ee617c28abd7bf9a0263bbe4b672429bf
Nixon Gamboa <nrgamboa97@gmail.com>
2021-07-26 15:45:14 -0500

    Se utiliza el modelo Animal del dominio para el servicio de crear animal

--- back/lib/application/animal/use-cases/create-animal.js
+++ back/lib/application/animal/use-cases/create-animal.js
@@ -1,6 +1,21 @@
-const Animal = require('../../../domain/animal/model/animal');
+const AgreggateId = require("../../../domain/agreggate-id");
+const AnimalRepository = require("../../../domain/animal/animal-repository");
+const Animal = require("../../../domain/animal/model/animal");
+const Age = require("../../../domain/animal/model/values/age");
+const Breed = require("../../../domain/animal/model/values/breed");
+const Gender = require("../../../domain/animal/model/values/gender");
 
-module.exports = ( {pastureId = null, breed, age, gender}, animalRepository ) => {
-  const animal = new Animal(pastureId, breed, age, gender);
-  return animalRepository.createAnimal(animal, animalRepository);
+module.exports = (
+  { pastureId = null, breed, age, gender },
+  animalRepository
+) => {
+  const pastureIdOV = pastureId ? new AgreggateId(pastureId) : null;
+  const animal = new Animal(
+    null,
+    new Breed(breed),
+    new Age(age),
+    new Gender(gender)
+  );
+  animal.associatePasture(pastureIdOV);
+  return animalRepository.createAnimal(animal);
 };
--- back/lib/infrastruture/webserver/animals-controllers/create-animal-controller.js
+++ back/lib/infrastruture/webserver/animals-controllers/create-animal-controller.js
@@ -1,15 +1,20 @@
 const animalsService = require("../../../application/animal/use-cases/index");
-const animalRepository = require("../../repositories/mongo/animal-repository")
+const AnimalRepository = require("../../repositories/mongo/animal-repository");
+const animalRepository = new AnimalRepository();
 
 module.exports = async (req, res, next) => {
-    const { body: animal } = req;
-    try {
-      const createdAnimalId = await animalsService.createAnimal( animal , animalRepository);
-      res.status(201).json({
-        dataId: createdAnimalId,
-        message: 'animal created',
-      });
-    } catch (err) {
-      next(err);
-    }
+  const { body: animal } = req;
+  try {
+    console.log(animal);
+    const createdAnimalId = await animalsService.createAnimal(
+      animal,
+      animalRepository
+    );
+    res.status(201).json({
+      dataId: createdAnimalId,
+      message: "animal created",
+    });
+  } catch (err) {
+    next(err);
   }
+};
